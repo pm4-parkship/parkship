c296b7508cd131e49e2d9c31b346859c5f393d3e
meierr02 <meierr02@students.zhaw.ch>
2023-04-01 15:43:37 +0200

    addded date fields

--- backend/src/main/java/ch/zhaw/parkship/parking/dto/ParkingLotSearchDto.java
+++ backend/src/main/java/ch/zhaw/parkship/parking/dto/ParkingLotSearchDto.java
@@ -5,13 +5,21 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Getter
 @Setter
 public class ParkingLotSearchDto implements Serializable {
 
     private String searchTerm;
-    public ParkingLotSearchDto (String searchTerm){
-        searchTerm = this.searchTerm;
+    private Date startDate;
+    private Date endDate;
+
+    public ParkingLotSearchDto (String searchTerm, Date startDate, Date endDate){
+
+        this.searchTerm = searchTerm;
+        this.startDate = startDate;
+        this.endDate = endDate;
+
     }
 }
