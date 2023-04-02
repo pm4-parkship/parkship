d2b756477463fee23ac3f30edb1afdfeaf6fb5a7
chrewbroccoli <christoph.walser@pm.me>
2023-04-02 21:47:52 +0200

    replaced java.util.date with java.time.ZonedDateTime

--- backend/src/main/java/ch/zhaw/parkship/dtos/ParkingLotSearchDto.java
+++ backend/src/main/java/ch/zhaw/parkship/dtos/ParkingLotSearchDto.java
@@ -5,17 +5,17 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.time.ZonedDateTime;
 
 @Getter
 @Setter
 public class ParkingLotSearchDto implements Serializable {
 
     private String searchTerm;
-    private Date startDate;
-    private Date endDate;
+    private ZonedDateTime startDate;
+    private ZonedDateTime endDate;
 
-    public ParkingLotSearchDto (String searchTerm, Date startDate, Date endDate){
+    public ParkingLotSearchDto (String searchTerm, ZonedDateTime startDate, ZonedDateTime endDate){
 
         this.searchTerm = searchTerm;
         this.startDate = startDate;
