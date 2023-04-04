e4c3ee14ebb27b8f41a7aeadf49ad1bf16774553
meierr02 <meierr02@students.zhaw.ch>
2023-04-04 13:37:08 +0200

    changed h2 to in memory db, added basic seed

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into "users"("email","name","password","surname") values ('robin.meier','Robin','1234','Meier');
+
+insert into "parking_lot"("address","address_nr","description","floor","latitude","longitude","nr","price","state","user_id") values('sdfsdf','4','ein guter parkplatz',3,4.0,6.0,'5',3.0,'frei',1);
