a8ddce14f5f7a555cedf7f0f12d0752df2e899e5
Sandro Guerotto <s.guerotto@gmail.com>
2023-04-09 16:34:05 +0200

    feat[PM4-55] added toast for cancel

--- frontend/src/components/my-reservation/reservation-table.tsx
+++ frontend/src/components/my-reservation/reservation-table.tsx
@@ -1,20 +1,32 @@
 import TableComponent from '../table/table-component';
 import { logger } from '../../logger';
+import { toast } from 'react-toastify';
+import { useTheme } from '@mui/styles';
 
 interface reservationTableProps {
   reservations: Array<string[]>;
 }
 
+const headerNames = [
+  'ID',
+  'Bezeichnung',
+  'Ort',
+  'Besitzer',
+  'gebucht von bis',
+  'storniert'
+];
+
 const ReservationTable = ({ reservations }: reservationTableProps) => {
-  const headerNames = [
-    'ID',
-    'Bezeichnung',
-    'Ort',
-    'Besitzer',
-    'gebucht von bis',
-    'storniert'
-  ];
-  const onCellClick = new Map([['stornieren', (e: any) => alert(e)]]);
+  const { palette } = useTheme();
+  const cancelReservation = (e: any) => {
+    toast.success('🦄 Wow so easy!', {
+      // theme: palette.mode
+    });
+  };
+
+  const onCellClick = new Map([
+    ['stornieren', (e: any) => cancelReservation(e)]
+  ]);
 
   return (
     <TableComponent
