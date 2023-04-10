0b9ff668b083ddbac99341aa7de84578ee794aa0
nadine-zhaw <moserna2@students.zhaw.ch>
2023-04-10 13:38:59 +0200

    add required infos to paper component

--- frontend/src/components/parking-list-item/parking-list-item.tsx
+++ frontend/src/components/parking-list-item/parking-list-item.tsx
@@ -1,8 +1,16 @@
+import { Paper, Typography } from "@mui/material";
 import { ParkingLotModel } from "src/models";
 
 const ParkingListItem = ({parking} : {parking : ParkingLotModel}) => {
   return (
-    <div>{parking.description}</div>
+    <Paper elevation={1}>
+        <Typography>Parkplatz {parking.nr}</Typography>
+        <br/>
+        <Typography>{parking.floor}</Typography>
+        <Typography>{parking.address}</Typography>
+        <Typography>{parking.addressNr}</Typography>
+        <Typography>CHF {parking.price} / Tag</Typography>
+    </Paper>
   );
 };
 
