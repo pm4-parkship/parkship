7e701c228cb41a3065006a1b03cfde19f88cd2f7
nadine-zhaw <moserna2@students.zhaw.ch>
2023-04-16 20:50:40 +0200

    add circular progress

--- frontend/pages/test/index.tsx
+++ frontend/pages/test/index.tsx
@@ -1,114 +1,134 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { makeStyles } from '@mui/styles';
 import { ParkingLotModel } from '../../src/models';
 import ParkingDetailModal from '../../src/components/parking-detail-modal/parking-detail-modal';
 import ParkingList from 'src/components/parking-list/parking-list';
+import { logger } from 'src/logger';
+import { CircularProgress } from '@mui/material';
 
 const Index = () => {
   const classes = useStyles();
   const router = useRouter();
-  const [showModal, setShowModal] = useState(true);
+
+  const [showModal, setShowModal] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  });
 
   const parkingEmptyDummyData: ParkingLotModel[] = [];
   const parkingDummyData: ParkingLotModel[] = [
-   {
-    address: 'Dorfstrasse',
-    addressNr: '8',
-    description: 'Prakplatz ist ein kleiner Parkplatz, nicht für alle Autos geignet.',
-    floor: 123,
-    latitude: 0,
-    longitude: 0,
-    nr: 'A2',
-    owner: 'Benjamin Blümchen',
-    pictures: [
-      'https://placehold.co/600x600.png',
-      'https://placehold.co/600x600.png'
-    ],
-    price: 1200,
-    state: 'busy',
-    tags: ['nice view', 'nice view', 'nice view', 'nice view']
-  },
-  {
-    address: 'Dorfstrasse',
-    addressNr: '8',
-    description: 'Prakplatz ist ein kleiner Parkplatz, nicht für alle Autos geignet.',
-    floor: 123,
-    latitude: 0,
-    longitude: 0,
-    nr: 'A2',
-    owner: 'Benjamin Blümchen',
-    pictures: [
-      'https://placehold.co/600x600.png',
-      'https://placehold.co/600x600.png'
-    ],
-    price: 1200,
-    state: 'busy',
-    tags: ['nice view', 'nice view', 'nice view', 'nice view']
-  },
-  {
-    address: 'Dorfstrasse',
-    addressNr: '8',
-    description: 'Prakplatz ist ein kleiner Parkplatz, nicht für alle Autos geignet.',
-    floor: 123,
-    latitude: 0,
-    longitude: 0,
-    nr: 'A2',
-    owner: 'Benjamin Blümchen',
-    pictures: [
-      'https://placehold.co/600x600.png',
-      'https://placehold.co/600x600.png'
-    ],
-    price: 1200,
-    state: 'busy',
-    tags: ['nice view', 'nice view', 'nice view', 'nice view']
-  },
-  {
-    address: 'Dorfstrasse',
-    addressNr: '8',
-    description: 'Prakplatz ist ein kleiner Parkplatz, nicht für alle Autos geignet.',
-    floor: 123,
-    latitude: 0,
-    longitude: 0,
-    nr: 'A2',
-    owner: 'Benjamin Blümchen',
-    pictures: [
-      'https://placehold.co/600x600.png',
-      'https://placehold.co/600x600.png'
-    ],
-    price: 1200,
-    state: 'busy',
-    tags: ['nice view', 'nice view', 'nice view', 'nice view']
-  },
-  {
-    address: 'Dorfstrasse',
-    addressNr: '8',
-    description: 'Prakplatz ist ein kleiner Parkplatz, nicht für alle Autos geignet.',
-    floor: 123,
-    latitude: 0,
-    longitude: 0,
-    nr: 'A2',
-    owner: 'Benjamin Blümchen',
-    pictures: [
-      'https://placehold.co/600x600.png',
-      'https://placehold.co/600x600.png'
-    ],
-    price: 1200,
-    state: 'busy',
-    tags: ['nice view', 'nice view', 'nice view', 'nice view']
-  }
-];
+    {
+      address: 'Dorfstrasse',
+      addressNr: '8',
+      description:
+        'Prakplatz ist ein kleiner Parkplatz, nicht für alle Autos geignet.',
+      floor: 123,
+      latitude: 0,
+      longitude: 0,
+      nr: 'A2',
+      owner: 'Benjamin Blümchen',
+      pictures: [
+        'https://placehold.co/600x600.png',
+        'https://placehold.co/600x600.png'
+      ],
+      price: 1200,
+      state: 'busy',
+      tags: ['nice view', 'nice view', 'nice view', 'nice view']
+    },
+    {
+      address: 'Dorfstrasse',
+      addressNr: '8',
+      description:
+        'Prakplatz ist ein kleiner Parkplatz, nicht für alle Autos geignet.',
+      floor: 123,
+      latitude: 0,
+      longitude: 0,
+      nr: 'A2',
+      owner: 'Benjamin Blümchen',
+      pictures: [
+        'https://placehold.co/600x600.png',
+        'https://placehold.co/600x600.png'
+      ],
+      price: 1200,
+      state: 'busy',
+      tags: ['nice view', 'nice view', 'nice view', 'nice view']
+    },
+    {
+      address: 'Dorfstrasse',
+      addressNr: '8',
+      description:
+        'Prakplatz ist ein kleiner Parkplatz, nicht für alle Autos geignet.',
+      floor: 123,
+      latitude: 0,
+      longitude: 0,
+      nr: 'A2',
+      owner: 'Benjamin Blümchen',
+      pictures: [
+        'https://placehold.co/600x600.png',
+        'https://placehold.co/600x600.png'
+      ],
+      price: 1200,
+      state: 'busy',
+      tags: ['nice view', 'nice view', 'nice view', 'nice view']
+    },
+    {
+      address: 'Dorfstrasse',
+      addressNr: '8',
+      description:
+        'Prakplatz ist ein kleiner Parkplatz, nicht für alle Autos geignet.',
+      floor: 123,
+      latitude: 0,
+      longitude: 0,
+      nr: 'A2',
+      owner: 'Benjamin Blümchen',
+      pictures: [
+        'https://placehold.co/600x600.png',
+        'https://placehold.co/600x600.png'
+      ],
+      price: 1200,
+      state: 'busy',
+      tags: ['nice view', 'nice view', 'nice view', 'nice view']
+    },
+    {
+      address: 'Dorfstrasse',
+      addressNr: '8',
+      description:
+        'Prakplatz ist ein kleiner Parkplatz, nicht für alle Autos geignet.',
+      floor: 123,
+      latitude: 0,
+      longitude: 0,
+      nr: 'A2',
+      owner: 'Benjamin Blümchen',
+      pictures: [
+        'https://placehold.co/600x600.png',
+        'https://placehold.co/600x600.png'
+      ],
+      price: 1200,
+      state: 'busy',
+      tags: ['nice view', 'nice view', 'nice view', 'nice view']
+    }
+  ];
 
   return (
     <div className={classes.superRoot}>
+      {isLoading ?? 
+        <div className={classes.progressContainer}>
+          <CircularProgress color="primary" />{' '}
+        </div>}
+
+        <ParkingList parkings={parkingDummyData} />
+      
+
       <ParkingDetailModal
         showModal={showModal}
         setShowModal={setShowModal}
         parkingLotModel={parkingDummyData[0]}
       />
-
-      <ParkingList parkings={parkingDummyData}/>
-
     </div>
   );
 };
@@ -117,6 +137,14 @@ const useStyles = makeStyles((theme) => ({
   superRoot: {
     maxWidth: '2000px',
     margin: '0 auto'
+  },
+  progressContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    verticalAlign: 'middle',
+    height: 150
   }
 }));
 
