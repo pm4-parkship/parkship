71a8ab490669d5a8616c281909c88db15acd90c0
chrewbroccoli <christoph.walser@pm.me>
2023-04-17 20:37:52 +0200

    added getByuserid method

--- backend/src/main/java/ch/zhaw/parkship/reservation/ReservationRepository.java
+++ backend/src/main/java/ch/zhaw/parkship/reservation/ReservationRepository.java
@@ -3,12 +3,18 @@ package ch.zhaw.parkship.reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.awt.print.Pageable;
+import java.time.ZonedDateTime;
+import java.util.List;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<ReservationEntity, Long> {
+    @Query ("SELECT r FROM ReservationEntity r WHERE r.tenant = ?1 AND r.from > ?2 ORDER BY r.from")
+    List<ReservationEntity> findAllByTenant(Long id, ZonedDateTime currentTime, Pageable pageable);
+
     @Query(
             "SELECT r FROM ReservationEntity r WHERE r.id = ?1 AND r.to >= ?2 AND r.from <= ?3"
     )
