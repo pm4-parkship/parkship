a6cfce5ef050199180946135fd2820a7f0fedf74
meierr02 <meierr02@students.zhaw.ch>
2023-05-06 09:55:41 +0200

    added tests for offers

--- backend/src/test/java/ch/zhaw/parkship/controllers/ReservationControllerTest.java
+++ backend/src/test/java/ch/zhaw/parkship/controllers/ReservationControllerTest.java
@@ -38,7 +38,6 @@ class ReservationControllerTest {
     private ReservationService reservationService;
     @Mock
     private AvailabilityService availabilityService;
-
     @Mock
     private ParkingLotRepository parkingLotRepository;
 
