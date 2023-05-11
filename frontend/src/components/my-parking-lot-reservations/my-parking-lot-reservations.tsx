d6dc6cfe8edf482c9b2d41af4816baea8a79e8d7
nadine-zhaw <moserna2@students.zhaw.ch>
2023-05-11 12:50:48 +0200

    feat [PM4-60] refactor code

---
+++ frontend/src/components/my-parking-lot-reservations/my-parking-lot-reservations.tsx
@@ -0,0 +1,78 @@
+
+import TableContainer from '@mui/material/TableContainer';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHeader from '../../../src/components/table/table-header';
+import TableBody from '@mui/material/TableBody';
+import { makeStyles } from '@mui/styles';
+import { TableCell, TableRow } from '@mui/material';
+import { MyParkingLotsTableProps } from 'pages/admin/my-parking-lots';
+import { nanoid } from 'nanoid';
+
+interface MyParkinLotReservationTableProps {
+  headerNames: string[];
+  reservations: MyParkingLotsTableProps[]
+}
+
+const MyParkinLotReservationTable = ({headerNames, reservations} : MyParkinLotReservationTableProps) => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }}>
+          <TableHeader headerNames={headerNames} />
+
+          <TableBody>
+            {reservations.map((row, index) => {
+              return (
+                <TableRow
+                  key={row.id}
+                  className={!row.active ? classes.baseRow : classes.activeRow}
+                >
+                  <TableCell align={'left'} variant={'body'} key={nanoid()}>
+                    {row.id}
+                  </TableCell>
+
+                  <TableCell align={'right'} variant={'body'} key={nanoid()}>
+                    {row.parkingLotName}
+                  </TableCell>
+
+                  <TableCell align={'right'} variant={'body'} key={nanoid()}>
+                    {row.location}
+                  </TableCell>
+
+                  <TableCell align={'right'} variant={'body'} key={nanoid()}>
+                    {row.reservedBy}
+                  </TableCell>
+
+                  <TableCell align={'right'} variant={'body'} key={nanoid()}>
+                    {row.bookingTime}
+                  </TableCell>
+
+                  <TableCell align={'right'} variant={'body'} key={nanoid()}>
+                    {row.status}
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
+
+const useStyles = makeStyles((theme) => ({
+  baseRow: {
+    width: '100%'
+  },
+  activeRow: {
+    backgroundColor: theme.palette.primary.main,
+    '&:hover': {
+      backgroundColor: theme.palette.primary.main
+    }
+  }
+}));
+
+export default MyParkinLotReservationTable;
